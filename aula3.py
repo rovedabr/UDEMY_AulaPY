786835178d160a2acf13be2298ccea585a9221e1
rovedabr <rovedabr@gmail.com>
2023-05-04 11:05:27 -0300

    aula3

---
+++ aula3.py
@@ -0,0 +1,9 @@
+#Tipo de dados
+#Strings - textos
+#Aspas simples / duplas
+print('teste'), print("teste2 teste três")
+#Escape
+print("teste \"teste")
+print(r"teste de escape\'teste") #mostrando o \ - raramente usado
+#forma mais usada
+print(2, "teste1 'teste2")
