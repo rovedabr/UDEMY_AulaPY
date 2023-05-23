1d475c5460602faf3bd0fc59445873c592755991
rovedabr <rovedabr@gmail.com>
2023-05-23 19:14:29 -0300

    iteração

---
+++ aula40.py
@@ -0,0 +1,11 @@
+texto = 'Python'
+for letra in texto:
+    print(letra)
+
+numeros = range(10)
+for numero in numeros:
+    print(numero)
+
+numeros2 = range(0, 10, 1)
+for numero2 in numeros2:
+    print(numero2)
