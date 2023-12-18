d469dc88d13b61c744e8e903506483071bd4b288
rovedabr <rovedabr@gmail.com>
2023-12-18 16:31:54 -0300

    Aula 49

--- aula48.py
+++ aula48.py
@@ -5,3 +5,17 @@ lista[2] = 300
 print(lista)
 del lista[3]
 print(lista)
+lista.append(50)
+print(lista)
+lista.append(70)
+print(lista)
+
+lista.pop() #remove o último valor ou o valor do índice indicado
+print(lista)
+
+del lista[-1]
+print(lista)
+
+lista.insert(0,'teste')
+print(lista)
+
---
+++ aula49.py
@@ -0,0 +1,34 @@
+list_a = [1,2,3]
+list_b = [4,5,6]
+
+list_c = list_a + list_b
+print(list_c)
+list_a.extend(list_b) #altera a própria lista a juntando os dados da lista b
+print(list_a)
+
+name = 'Pedro'
+other_name = name
+print(other_name)
+
+#alterando dados do "other_name"
+other_name = 'Maria'
+print(name)
+print(other_name)
+
+#--------------------------------------------
+
+new_list_a = ['Maria', 1, 2, 3, 1.2, True]
+print(new_list_a)
+
+new_list_b = new_list_a.copy()
+print(new_list_b)
+
+new_list_b[0] = 'Pedro'
+print(new_list_b)
+
+#--------------------------------------------
+list_c = ['Maria', 'Ana', 'Pedro', 1, 1.2, True]
+
+for name in list_c:
+  print(name, type(name))
+
