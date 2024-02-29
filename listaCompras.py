f5beb492bc1ca0317d868d2596785d4a9770dd60
rovedabr <rovedabr@gmail.com>
2024-02-29 14:21:32 -0300

    finalizado programa de listas

--- listaCompras.py
+++ listaCompras.py
@@ -10,8 +10,21 @@ while True:
     valor = input('Valor: ')
     lista.append(valor)
   elif opcao == 'a':
-    print('a')
+    indice_str = input('Escolha o índice que quer apagar: ')
+    try:
+      indice = int(indice_str)
+      del lista[indice]
+      print('Item apagado com sucesso da lista')
+    except:
+      print('Não foi possível apagar este índice')
+
   elif opcao == 'l':
-    print('l')
-  else:
-    print("Escolha uma das opções descritas (i, a, l)")
+    os.system('clear')
+
+    if len('lista') == 0:
+      print('Nada para mostrar')
+
+    for i, valor in enumerate(lista):
+      print(i, valor)
+    else:
+     print("Escolha uma das opções descritas (i, a, l)")
